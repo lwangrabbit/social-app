9d4821914d6eefed9b84f76afbed7d25a80ce58d
wanglipeng <wanglipeng.007@gmail.com>
2024-09-11 14:03:34 +0800

    update

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,104 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+import { title } from 'process'
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+  title: 'Frog Frame',
+})
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame("/", async (c) => {
+  return c.res({
+    action: "/finish",
+    image:
+      "https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/37e16d62-efa5-4dca-8e6d-dfd7be477c00/rectcontain3",
+    imageAspectRatio: "1:1",
+    intents: [
+      <Button.Link href="https://docs.linea.build/">Linea Docs</Button.Link>,
+      <Button.Link href="https://bridge.linea.build/">Bridge your funds？🚀</Button.Link>
+    ],
+    title: "Buy ETH",
+  });
+  // }
+});
+
+
+// app.frame('/', (c) => {
+//   const { buttonValue, inputText, status } = c
+//   const fruit = inputText || buttonValue
+//   return c.res({
+//     image: (
+//       <div
+//         style={{
+//           alignItems: 'center',
+//           background:
+//             status === 'response'
+//               ? 'linear-gradient(to right, #432889, #17101F)'
+//               : 'black',
+//           backgroundSize: '100% 100%',
+//           display: 'flex',
+//           flexDirection: 'column',
+//           flexWrap: 'nowrap',
+//           height: '100%',
+//           justifyContent: 'center',
+//           textAlign: 'center',
+//           width: '100%',
+//         }}
+//       >
+//         <div
+//           style={{
+//             color: 'white',
+//             fontSize: 60,
+//             fontStyle: 'normal',
+//             letterSpacing: '-0.025em',
+//             lineHeight: 1.4,
+//             marginTop: 30,
+//             padding: '0 120px',
+//             whiteSpace: 'pre-wrap',
+//           }}
+//         >
+//           {status === 'response'
+//             ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+//             : 'Welcome!'}
+//         </div>
+//       </div>
+//     ),
+//     intents: [
+//       <TextInput placeholder="Enter custom fruit..." />,
+//       <Button value="apples">Apples</Button>,
+//       <Button value="oranges">Oranges</Button>,
+//       <Button value="bananas">Bananas</Button>,
+//       status === 'response' && <Button.Reset>Reset</Button.Reset>,
+//     ],
+//   })
+// })
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
+
+// NOTE: That if you are using the devtools and enable Edge Runtime, you will need to copy the devtools
+// static assets to the public folder. You can do this by adding a script to your package.json:
+// ```json
+// {
+//   scripts: {
+//     "copy-static": "cp -r ./node_modules/frog/_lib/ui/.frog ./public/.frog"
+//   }
+// }
+// ```
+// Next, you'll want to set up the devtools to use the correct assets path:
+// ```ts
+// devtools(app, { assetsPath: '/.frog' })
+// ```
